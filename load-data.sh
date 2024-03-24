8f6e0225c5a023b45b673cabc5be4712b881ea81
Maxwell Mendenhall <97015178+MaxwellMendenhall@users.noreply.github.com>
2024-03-24 15:41:03 -0400

    Create load-data.sh

---
+++ load-data.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Wait for MySQL to be up
+until mysql -h localhost -u root -p"my-secret-pw" -e "SELECT 1" mydatabase > /dev/null 2>&1; do
+    echo "Waiting for MySQL server..."
+    sleep 1
+done
+
+echo "MySQL server is up. Inserting data."
+
+# Insert data into MySQL
+mysql -h localhost -u root -p"my-secret-pw" mydatabase <<EOF
+LOAD DATA LOCAL INFILE '/var/lib/mysql-files/cattle_data.csv'
+INTO TABLE cattle
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+EOF
+
+echo "Data inserted successfully."
